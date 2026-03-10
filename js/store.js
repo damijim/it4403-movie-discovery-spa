402e9576e1419b7597a405e0ad5ae88a62d4ae5d
chenningksu <156732729+chenningksu@users.noreply.github.com>
2026-03-10 19:47:35 -0400

    Create store.js
    Created TMDB access and retrieved API key. Documented Key and login information in shared documents.

---
+++ js/store.js
@@ -0,0 +1,22 @@
+window.Store = {
+  config: {
+    apiKey: "7edb209ee6014a0d39422a95e0af0362",
+    baseUrl: "https://api.themoviedb.org/3",
+    imageBase: "https://image.tmdb.org/t/p/w500",
+    placeholderPoster: "https://via.placeholder.com/400x600?text=No+Poster"
+  },
+
+  state: {
+    currentView: "home",
+    homeMode: "popular", // popular | search
+    homePage: 1,
+    homeQuery: "",
+
+    discoverPage: 1,
+    discoverGenre: "",
+    discoverSort: "popularity.desc",
+
+    genresLoaded: false,
+    genres: []
+  }
+};
